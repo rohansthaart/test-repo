5e8cec6a459418a46fc1dfbd1c3c8b958a65c050
Rohan Shrestha <64144604+rohansthaart@users.noreply.github.com>
2025-08-22 16:14:52 +0545

    Update auth.schema.js

--- schemas/auth.schema.js
+++ schemas/auth.schema.js
@@ -6,6 +6,8 @@ exports.registerUserSchema = zod_1.z.object({
     name: zod_1.z.string().min(1, "Name is required"),
     email: zod_1.z.string().email("Invalid email address"),
     password: zod_1.z.string().min(4, "Password must be at least 4 characters long"),
+    hikingExperience: zod_1.z.string().optional(),
+    userRole: zod_1.z.enum(["USER", "GUIDE"]),
 });
 exports.loginUserSchema = zod_1.z.object({
     email: zod_1.z.string().email("Invalid email address"),
