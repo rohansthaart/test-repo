d872779fff01863e2ad40c857d8fd4f7f98a2456
Rohan Shrestha <64144604+rohansthaart@users.noreply.github.com>
2025-08-24 16:52:47 +0545

    Enhance auth routes with middleware for profile actions

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,11 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const tour_controller_1 = require("../controller/tour.controller");
 const auth_controller_1 = require("../controller/auth.controller");
+const auth_middleware_1 = require("../middleware/auth.middleware");
 const router = express_1.default.Router();
 router.route("/login").post(auth_controller_1.loginUser);
 router.route("/register").post(auth_controller_1.registerUser); // Assuming you want to register a user
 router.get("/allUsers", auth_controller_1.getAllUsers);
-router.get('/getGuideProfile/:id', auth_controller_1.getGuideProfile);
+router.get("/getGuideProfile/:id", auth_controller_1.getGuideProfile);
 router.route("/:id").get(tour_controller_1.getTourById);
 // Assuming you want to get a specific tour by ID
+router.route("/updateProfile").post(auth_middleware_1.authenticate, auth_controller_1.completeProfile);
+router.route("/getUserProfile").get(auth_middleware_1.authenticate, auth_controller_1.getMyProfileDetail);
 exports.default = router;
